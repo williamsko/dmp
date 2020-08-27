c85a0cc37efc934c8d76ae121f9a853a11f71fcb
Williams de SOUZA <williamsko89@gmail.com>
2020-08-27 16:28:10 +0200

    feature : write function to generate empty dossier number

---
+++ utils/utilities.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+const (
+	letterBytes   = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ" // 52 possibilities
+	letterIdxBits = 6                                      // 6 bits to represent 64 possibilities / indexes
+	letterIdxMask = 1<<letterIdxBits - 1                   // All 1-bits, as many as letterIdxBits
+)
+
+// RandomObjectMatricule : generate random matricule to usager, dossier etc ...
+func RandomObjectMatricule(length int) string {
+
+	result := make([]byte, length)
+	bufferSize := int(float64(length) * 1.3)
+	for i, j, randomBytes := 0, 0, []byte{}; i < length; j++ {
+		if j%bufferSize == 0 {
+			randomBytes = SecureRandomBytes(bufferSize)
+		}
+		if idx := int(randomBytes[j%length] & letterIdxMask); idx < len(letterBytes) {
+			result[i] = letterBytes[idx]
+			i++
+		}
+	}
+
+	return string(result)
+}
+
+// SecureRandomBytes returns the requested number of bytes using crypto/rand
+func SecureRandomBytes(length int) []byte {
+	var randomBytes = make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		panic("Unable to generate random bytes")
+	}
+	return randomBytes
+}
