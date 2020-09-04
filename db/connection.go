0908be0466e86584acc93691b7aacf82f742a26e
Williams de SOUZA <williamsko89@gmail.com>
2020-09-04 19:33:46 +0200

    refacto : refacto database connection method

--- db/connection.go
+++ db/connection.go
@@ -2,21 +2,33 @@ package db
 
 import (
 	"context"
-	"log"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 // ConnectDb : Database connexion method
 func ConnectDb() *mongo.Database {
-	ctx := context.Background()
-	client, err := mongo.Connect(
-		ctx,
-		options.Client().ApplyURI("mongodb://localhost:27017/todo"),
-	)
-	if err != nil {
+	client := InitiateMongoClient()
+	return client.Database("todo")
+}
+
+// ConnectGridFs : Database connexion method to GridFS
+func ConnectGridFs() *mongo.Database {
+	client := InitiateMongoClient()
+	return client.Database("dmpfiles")
+}
+
+// InitiateMongoClient : Database connexion method
+func InitiateMongoClient() *mongo.Client {
+	var err error
+	var client *mongo.Client
+	uri := "mongodb://localhost:27017/"
+	opts := options.Client()
+	opts.ApplyURI(uri)
+	opts.SetMaxPoolSize(5)
+	if client, err = mongo.Connect(context.Background(), opts); err != nil {
 		log.Fatal(err)
 	}
-	return client.Database("todo")
+	return client
 }
